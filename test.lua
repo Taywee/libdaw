7db2626b58dcc9dd65e732628304fb9b966c1943
Taylor C. Richberger <taywee@gmx.com>
2024-02-24 22:36:28 -0700

    fix click caused by Delay
    The Delay node was not making any output while waiting for its input
    buffer to fill.  This caused problems with the Multiply node.

--- test.lua
+++ test.lua
@@ -5,7 +5,6 @@ local sample_rate = 48000
 local graph = nodes.Graph()
 
 local function make_echo(args)
-  -- TODO: fix starting click
   local delay_seconds = args.delay or error("need a delay argument")
   local gain_ratio = args.gain or error("need a gain argument")
   local graph = nodes.Graph()
@@ -51,27 +50,26 @@ local function note(args)
     elseif sample == last then
       graph:remove(note_graph_index)
       daw.cancel_before_sample(handle)
-      print('cancelling')
     end
   end)
 end
 
 for i=0, 10 do 
-  note{ node = nodes.SawtoothOscillator(), note = 0, delay = i * 4, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 4, delay = i * 4, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 7, delay = i * 4, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 0, delay = i * 4, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 4, delay = i * 4, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 7, delay = i * 4, length = 0.1 }
 
-  note{ node = nodes.SawtoothOscillator(), octave = 3, note = 11, delay = i * 4 + 1, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 2, delay = i * 4 + 1, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 7, delay = i * 4 + 1, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), octave = 3, note = 11, delay = i * 4 + 1, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 2, delay = i * 4 + 1, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 7, delay = i * 4 + 1, length = 0.1 }
 
-  note{ node = nodes.SawtoothOscillator(), note = 0, delay = i * 4 + 2, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 4, delay = i * 4 + 2, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 9, delay = i * 4 + 2, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 0, delay = i * 4 + 2, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 4, delay = i * 4 + 2, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 9, delay = i * 4 + 2, length = 0.1 }
 
-  note{ node = nodes.SawtoothOscillator(), note = 0, delay = i * 4 + 3, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 5, delay = i * 4 + 3, length = 0.1 }
-  note{ node = nodes.SawtoothOscillator(), note = 9, delay = i * 4 + 3, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 0, delay = i * 4 + 3, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 5, delay = i * 4 + 3, length = 0.1 }
+  note{ node = nodes.SquareOscillator(), note = 9, delay = i * 4 + 3, length = 0.1 }
 end
 
 return graph
